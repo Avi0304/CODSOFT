189469150c6fb584edcf5635c8cf9f9c50ed6bba
Avi0304 <157276472+Avi0304@users.noreply.github.com>
2024-05-11 18:55:22 +0530

    complete all task

---
+++ TASK-2 (PORTFOLIO)/main.js
@@ -0,0 +1,30 @@
+var typed = new Typed(".text",{
+    strings: ["Full stack Web Developer"],
+    typeSpeed: 100,
+    // backSpeed: 100,
+    // loop: true
+})
+
+const navLinks = document.querySelectorAll('.navbar a');
+navLinks.forEach(link => {
+    link.addEventListener('click', (e) => {
+        e.preventDefault();
+        const targetId = link.getAttribute('href').substring(1);
+        const targetSection = document.getElementById(targetId);
+        const offset = 100; // Adjust this value as needed
+        window.scrollTo({
+            top: targetSection.offsetTop - offset,
+            behavior: 'smooth'
+        });
+    });
+});
+
+
+const navbarLinks = document.querySelectorAll('.navbar a');
+
+navbarLinks.forEach(link => {
+    link.addEventListener('click', () => {
+        navbarLinks.forEach(item => item.classList.remove('active'));
+        link.classList.add('active');
+    });
+});
