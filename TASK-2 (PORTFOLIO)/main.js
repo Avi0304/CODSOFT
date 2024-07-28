0c2c32a3dcff7581d845906af9b64730ce145734
Avi0304 <157276472+Avi0304@users.noreply.github.com>
2024-07-28 18:32:32 +0530

    CODSOFT

--- TASK-2 (PORTFOLIO)/main.js
+++ TASK-2 (PORTFOLIO)/main.js
@@ -1,8 +1,7 @@
 var typed = new Typed(".text",{
     strings: ["Full stack Web Developer"],
     typeSpeed: 100,
-    // backSpeed: 100,
-    // loop: true
+    
 })
 
 const navLinks = document.querySelectorAll('.navbar a');
--- TASK-3 (CALCULATOR)/script.js
+++ TASK-3 (CALCULATOR)/script.js
@@ -1,38 +1,3 @@
-// let input = document.getElementById('inp');
-// let btn = document.querySelectorAll('button');
-
-// let string='';
-// let arr= Array.from(btn);
-// arr.forEach(button => {
-//     button.addEventListener('click', (e)=>{
-//         if(e.target.innerHTML == '='){
-//             string = eval(string);
-//             input.value = string;
-//         }
-//         else if(e.target.innerHTML == 'AC'){
-//             string = '';
-//             input.value = string;
-//         }
-//         else if(e.target.innerHTML == 'DEL'){
-//             string = string.substring(0,string.length-1);
-//             input.value = string;
-//         }
-//         else if (e.target.innerHTML === '.') {
-//             if (!decimalAdded) {
-//                 string += e.target.innerHTML;
-//                 input.value = string;
-//                 decimalAdded = true;
-//             }
-//         } else {
-//             string += e.target.innerHTML;
-//             input.value = string;
-//             decimalAdded = false; // Reset the decimalAdded flag for new number entry
-//         }
-//     })
-// })
-
-
-
 let input = document.getElementById('inp');
 let buttons = document.querySelectorAll('button');
 
